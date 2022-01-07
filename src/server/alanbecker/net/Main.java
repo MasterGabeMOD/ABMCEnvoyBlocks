6a70b26139bd88a681a665367099f128d04154eb
MasterGabeMOD <MasterGabeMod@yahoo.com>
2022-01-06 22:55:10 -0800

    Shovel
    This just uploads fixes on what we did ages ago. Also adds protections against shovels

--- src/server/alanbecker/net/Main.java
+++ src/server/alanbecker/net/Main.java
@@ -1,17 +1,19 @@
 package server.alanbecker.net;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Effect;
 import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
-    static World Envoy = Bukkit.getWorld("Envoy");
+	
 
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
@@ -23,14 +25,44 @@ public class Main extends JavaPlugin implements Listener {
     }
 
 
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler
     public void onPlace(BlockPlaceEvent e) {
-        if (e.getPlayer().getWorld() == Envoy){
-            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, () -> {
-                e.getBlock().setType(Material.AIR);
+      if (e.getPlayer().getWorld() == Bukkit.getWorld("Envoy"))
+        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask((Plugin)this, () -> {
+              e.getBlock().setType(Material.AIR);
                 e.getBlock().getWorld().playEffect(e.getBlock().getLocation(), org.bukkit.Effect.STEP_SOUND, 49);
+                e.getBlock().getWorld().playEffect(e.getBlock().getLocation(), Effect.DRAGON_BREATH, 49);
                 System.out.println("wowi");
             }, 15 * 10);
         }
-    }
+   
+
+@EventHandler
+public void onFarmland(PlayerInteractEvent e) {
+  Player p = e.getPlayer();
+  Material s = p.getInventory().getItemInMainHand().getType();
+  Material st = p.getInventory().getItemInOffHand().getType();
+  if (e.getPlayer().getWorld() == Bukkit.getWorld("Envoy") && 
+    e.getAction().equals(Action.RIGHT_CLICK_BLOCK))
+    if (s.equals(Material.WOODEN_HOE) || s.equals(Material.STONE_HOE) || s.equals(Material.GOLDEN_HOE) || s.equals(Material.DIAMOND_HOE) || s.equals(Material.NETHERITE_HOE)) {
+      e.setCancelled(true);
+    } else if (st.equals(Material.WOODEN_HOE) || st.equals(Material.STONE_HOE) || st.equals(Material.GOLDEN_HOE) || st.equals(Material.DIAMOND_HOE) || st.equals(Material.NETHERITE_HOE)) {
+      e.setCancelled(true);
+    }  
+}
+
+
+@EventHandler
+public void onFarm(PlayerInteractEvent e) {
+  Player p = e.getPlayer();
+  Material s = p.getInventory().getItemInMainHand().getType();
+  Material st = p.getInventory().getItemInOffHand().getType();
+  if (e.getPlayer().getWorld() == Bukkit.getWorld("Envoy") && 
+    e.getAction().equals(Action.RIGHT_CLICK_BLOCK))
+    if (s.equals(Material.WOODEN_SHOVEL) || s.equals(Material.STONE_SHOVEL) || s.equals(Material.GOLDEN_SHOVEL) || s.equals(Material.DIAMOND_SHOVEL) || s.equals(Material.NETHERITE_SHOVEL)) {
+      e.setCancelled(true);
+    } else if (st.equals(Material.WOODEN_SHOVEL) || st.equals(Material.STONE_SHOVEL) || st.equals(Material.GOLDEN_SHOVEL) || st.equals(Material.DIAMOND_SHOVEL) || st.equals(Material.NETHERITE_SHOVEL)) {
+      e.setCancelled(true);
+    }  
+}
 }
